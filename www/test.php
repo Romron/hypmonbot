6c490b9a0c2db9f6ee868eea939b8e7789b9086a
Romron <romron@ukr.net>
2017-08-16 13:44:49 +0300

    Дата добавляеться в БД и извлекаеться в нормальном формате

--- www/test.php
+++ www/test.php
@@ -20,23 +20,16 @@
 	
 	// queryInputIntoDB($link_DB,$ArrNameHyp);
 
-	// $date_today = date('m-d-y', time('h:i:s'));
-	// $date_today = date('U');
-	$date_today = date('Y-d-m');
+	$date_today = time();	//	получаем текушее кол-во секунд в эпохе Юникс
 
-	echo $date_today;
+	echo "<br>".$date_today."&nbsp;&nbsp;Отправленно в БД";
 
 	$query_input = "INSERT INTO test_2(`date`) VALUES ('".$date_today."')";
-	mysqli_query($link_DB,$query_input) or die("Query failed : " . mysqli_error($link_DB));
+		mysqli_query($link_DB,$query_input) or die("Query failed : " . mysqli_error($link_DB));
+	$query_D = "SELECT (`date`) FROM test_2";
+		$result_D = mysqli_query($link_DB,$query_D) or die("Query failed : " . mysql_error());
 
-
-	echo "<br>";
-	echo "<br>";
-	echo "<br>";
-
-	$result = querySelectIntoDB($link_DB);
-	OutputResultSQL($result);
-
+	echo "<br>".date('d.m.y H:i:s', time($result_D))."&nbsp;&nbsp;Получено из базы";		//	преобразовываем кол-во секунд в эпохе Юникс в норм формат
 
 	/* Закрываем соединение с базой данных*/
 	mysqli_close($link_DB);
