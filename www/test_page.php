8051f1b4a06b8f424634b95290853d3bb6ab95f4
Romron <romron@ukr.net>
2017-11-29 16:54:39 +0200

    парсинг фин данных с монитора: http://list4hyip.com 1.3

--- www/test_page.php
+++ www/test_page.php
@@ -19,15 +19,22 @@
 
 
 
-	$patern_3_0 = '#<div class="main-col" style="position: relative;">(.*)<div class="col3">#sU'; 	// получаем блок проэкта
+	// $patern_3_0 = '#<div class="main-col" style="position: relative;">(?:<b class="min">\D*(\d{1,4}\.?\d*)[\s\D]*.*<\/b>)(?:<b class="min">\D*(\d{1,4}\.?\d*)[\s\D]*.*<\/b>)<div class="col3">#sU'; 	// получаем блок проэкта
+	// $patern_3_0 = '#<div class="main-col" style="position: relative;">.*(<a.*target="_blank">.*<img src=.*(?!list4hyip.com)(https?://(?!mozshot.nemui.org).*)/).*<div class="col3">#sU'; 	// получаем название проэкта
+	// $patern_3_0 = '#(?:<b class="min">\$?(\d{1,4}\.?\d{1,4})[\s\D\<]+)#sU'; 	// минимальный вклад
+	
+
+	$patern_3_0 = '~(<div class="main-col".*(class="plan-d">Plan: <b style="color:#FF0000;">[\w:\s]*(\d+\.?\d{0,2})[%\-\s,]).*(<a.*target="_blank">.*<img src=.*(?!list4hyip.com)(https?:\/\/(?!mozshot.nemui.org).*)\/).*(<b class="min">\$?(\d{1,4}\.?\d{1,4})[\s\D\<]+).*<div class="col3">)~sU'; 	// получаем блок проэкта
 	if (!preg_match_all($patern_3_0,$page_list4hyip,$result_3_0,PREG_PATTERN_ORDER)) { 
 	    echo "func GetHypNam:  patern_3_0 ненайден или ошибка";
 	    // return false;
 		} 
 
-		echo "<br>-------------------------------------------------------------------------------------------------<br>";
-			print_r($result_3_0);
-		echo "<br>-------------------------------------------------------------------------------------------------<br>";
+		// echo "<br>-------------------------------------------------------------------------------------------------<br>";
+		// 	print_r($result_3_0);
+		// echo "<br>-------------------------------------------------------------------------------------------------<br>";
+
+		echo "<br>".Build_tree_arr($result_3_0);
 
 	// $patern_3_1 = '#<a.*target="_blank">.*<img src=.*(?!list4hyip.com)(https?://(?!mozshot.nemui.org).*)/#sU'; 	// для тестов
 	// if (!preg_match_all($patern_3_1,$result_3,$result_3_1,PREG_PATTERN_ORDER)) { 
@@ -35,11 +42,13 @@
 	//     // return false;
 	// 	} 
 
-	// $patern_3_2 = '##';		
-	// 	if (!preg_match_all($patern_3_2,$result_3,$result_3_2,PREG_PATTERN_ORDER)) { 
+	// $patern_3_2 = '#<b class="min">\D*(\d{1,4}\.?\d*)[\s\D]*.*<\/b>#';		
+	// 	if (!preg_match_all($patern_3_2,$page_list4hyip,$result_3_2,PREG_PATTERN_ORDER)) { 
 	// 	    echo "func TEST:  patern_3_2 ненайден или ошибка<br>";
 	// 		// exit();
 	// 		} 		
+		// echo "<br>".Build_tree_arr($result_3_0);
+
 
 	// array_push($result_3,$patern_3_0,$patern_3_1,$patern_3_2);
 	// echo "<br>".Build_tree_arr($result_3);
