efff07077686eb21c718c01ba8f59e7cf0a73a09
Romron <romron@ukr.net>
2017-11-30 16:44:44 +0200

    парсинг фин данных с монитора: http://list4hyip.com 1.4

--- www/test_page.php
+++ www/test_page.php
@@ -22,9 +22,10 @@
 	// $patern_3_0 = '#<div class="main-col" style="position: relative;">(?:<b class="min">\D*(\d{1,4}\.?\d*)[\s\D]*.*<\/b>)(?:<b class="min">\D*(\d{1,4}\.?\d*)[\s\D]*.*<\/b>)<div class="col3">#sU'; 	// получаем блок проэкта
 	// $patern_3_0 = '#<div class="main-col" style="position: relative;">.*(<a.*target="_blank">.*<img src=.*(?!list4hyip.com)(https?://(?!mozshot.nemui.org).*)/).*<div class="col3">#sU'; 	// получаем название проэкта
 	// $patern_3_0 = '#(?:<b class="min">\$?(\d{1,4}\.?\d{1,4})[\s\D\<]+)#sU'; 	// минимальный вклад
+	// $patern_3_0 = '~(<div class="main-col".*(class="plan-d">Plan: <b style="color:#FF0000;">[\w:\s]*(\d+\.?\d{0,2})[%\-\s,]).*(<a.*target="_blank">.*<img src=.*(?!list4hyip.com)(https?:\/\/(?!mozshot.nemui.org).*)\/).*(<b class="min">\$?(\d{1,4}\.?\d{1,4})[\s\D\<]+).*<div class="col3">)~sU'; 	// получаем блок проэкта
 	
 
-	// $patern_3_0 = '~(<div class="main-col".*(class="plan-d">Plan: <b style="color:#FF0000;">[\w:\s]*(\d+\.?\d{0,2})[%\-\s,]).*(<a.*target="_blank">.*<img src=.*(?!list4hyip.com)(https?:\/\/(?!mozshot.nemui.org).*)\/).*(<b class="min">\$?(\d{1,4}\.?\d{1,4})[\s\D\<]+).*<div class="col3">)~sU'; 	// получаем блок проэкта
+
 	$patern_3_0 = '~<div class="main-col"(.*)<div class="col3">~sU'; 	// получаем блок проэкта
 	if (!preg_match_all($patern_3_0,$page_list4hyip,$result_3_0,PREG_PATTERN_ORDER)) { 
 	    echo "func GetHypNam:  patern_3_0 ненайден или ошибка";
@@ -38,19 +39,25 @@
 		// echo "<br>".Build_tree_arr($result_3_0);
 
 	
-	for ($i=0; $i < count($result_3_0[1]); $i++) { 
-		// echo "<br>**********************************************<br>";
-		// print_r($result_3_0[1][$i]);
+	for ($i=0; $i < 5/*count($result_3_0[1])*/; $i++) {	// В полученном блоке ищу нужыне мне параметры
+		
+		echo "<br>**********************************************<br>";
+		print_r($result_3_0[1][$i]);
 
-		$patern_3_1 = '#<a.*target="_blank">.*<img src=.*(?!list4hyip.com)(https?:\/\/(?!mozshot.nemui.org).*)\/\?refer=List4Hyip" width="125"#sU'; 	// для тестов
+		// $patern_3_1 = '#<a href="\/go\/lid\/\d+" target="_blank">\s*<img src="(?:https?:\/\/)[w]{0,3}\.?([\da-z\.-]+\.[a-z\.]{2,6})(?:.*\?https?:\/\/[w]{0,3}\.?([\da-z\.-]+\.[a-z\.]{2,6}))?#'; 	// для тестов
+		$patern_3_1 = '#<a href="\/go\/lid\/\d+" target="#'; 	// для тестов
 		if (!preg_match_all($patern_3_1,$result_3_0[1][$i],$result_3_1,PREG_PATTERN_ORDER)) { 
+		    echo "<br><br>";
 		    echo "func GetHypNam:  patern_3_1 ненайден или ошибка";
 		    // return false;
 			}
-		echo "<br>".Build_tree_arr($result_3_1);
+
+		echo "<br>";
+		print_r($result_3_1);
+		// echo "<br>".Build_tree_arr($result_3_1);
 
 
-	}
+		}
 
 
 	// $patern_3_2 = '#<b class="min">\D*(\d{1,4}\.?\d*)[\s\D]*.*<\/b>#';		
