dcf62310632607d94ec3a6e3d23e692ee0b45297
Romron <romron@ukr.net>
2018-01-05 09:11:22 +0200

    тестирую ф-цию сортировки 2

--- www/Crypto/funktions_hypmon.php
+++ www/Crypto/funktions_hypmon.php
@@ -667,7 +667,7 @@
 		// формируем и отправляем ВТОРОЙ запрос
 			for ($z=0; $z < count($result_1); $z++) { 
 			    $query_2 = "SELECT * FROM `".$name_table."` 
-			    			WHERE ".$main_field." = '".$result_1[$z][$main_field]."'
+			    			WHERE `".$sorting_field."` = '".$result_1[$z][$sorting_field]."'
 			    			";	
 			    // отправка ВТОРОГО запроса 
 				   	$result_query_SQL = mysqli_query($link_DB,$query_2);
