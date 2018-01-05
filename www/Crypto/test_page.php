10bef62e75e1d235ce27d7cf56020dfb39f9ce65
Romron <romron@ukr.net>
2018-01-05 09:12:25 +0200

    тестирую ф-цию сортировки 2_1

--- www/Crypto/test_page.php
+++ www/Crypto/test_page.php
@@ -14,11 +14,109 @@
 <body>
 
 <?php
+	// // ====================== MySQL запросы ======================
 
-	$name_table = "Crypto_test";
-	$link_DB = conect_DB();
 
-	querySortingFromDB($link_DB,$name_table,'Name','Frequency_Daily','DESC','0',1)
+		// $name_table = "Crypto_test";	//	Выбор таблицы в базе данных
+		$name_table = "Work_table_1";	//	Выбор таблицы в базе данных
+		// $name_table = "Work_table_3";	//	Выбор таблицы в базе данных
+		$link_DB = conect_DB();	
+
+		
+
+		$result = querySortingFromDB($link_DB,$name_table,'project','cy','DESC','0',1);
+
+	    echo "<br><b> result: </b>&nbsp;&nbsp;&nbsp;&nbsp;<br>";
+	    echo Build_tree_arr($result);
+
+
+
+
+
+
+
+
+
+
+
+
+	// 	// текст запроса
+
+	//     // $query = "SELECT 1+1 AS Result";
+	//     // $query = "SELECT * FROM `".$name_table."`";
+	//     // $query = "SELECT id, Date FROM `".$name_table."`";
+	//     // $query = "SELECT id AS N, Date AS QQ FROM `".$name_table."`";
+	//     // $query = "SELECT id, Date, Name, Frequency_1 FROM `".$name_table."` 
+	//     			// ORDER BY Name DESC
+	//     			// LIMIT 10,100";	    
+
+	//     // $query = "SELECT id, Date, Name, Frequency_1 FROM `".$name_table."` 
+	//     // 			WHERE Frequency_1 > 0
+	//     // 			ORDER BY Name 
+	//     // 			";	    
+
+	//     // $query = "SELECT DISTINCT Name FROM `".$name_table."` 
+	//     // 			ORDER BY Name 
+	//     // 			";
+	    
+	//     // $query = "SELECT * FROM `".$name_table."` 
+	//     // 			GROUP BY project
+	//     // 			ORDER BY cy DESC
+	//     // 			";	  	    
+
+	//     $query = "SELECT * FROM `".$name_table."` 
+	//     			WHERE project = 'indeliblegain.com'
+	//     			";	  
+
+
+
+
+
+	//     // вывод запроса
+	// 	    echo "<br><b> query: </b>&nbsp;&nbsp;&nbsp;&nbsp;";
+	// 	    print_r($query);
+	//     	echo "<br>========================================";
+
+	//     // отправка запроса 
+	// 	   	$result_query_SQL = mysqli_query($link_DB,$query);
+	// 	    if (!$result_query_SQL) {
+	// 	    	echo "<br><br><b><font color='red'>".__FUNCTION__."Query failed : " . mysqli_error($link_DB)."</font></b>";	
+	// 	    	exit();
+	// 	    	}    
+
+	//     // оброботка ответа сервера
+	// 		for ($i=0; $i < mysqli_num_rows($result_query_SQL); $i++) {
+	// 	    	if ($i>5000) { break; }
+	// 	   		$result[] = mysqli_fetch_assoc($result_query_SQL);
+	// 	    	}
+		
+	// 	// // вывод результатов в виде таблицы
+	// 	// 	echo "<table>";
+	// 	// 		foreach ($result[0] as $key => $value) {
+	// 	// 			echo "<th>".$key."</th>";
+	// 	// 			}
+
+	// 	// 		for ($q=0; $q < count($result); $q++) { 
+	// 	// 			echo "<tr>";
+	// 	// 			foreach ($result[$q] as $value) {
+
+	// 	// 				echo "<td>";
+	// 	// 					echo $value;
+	// 	// 				echo "</td>";
+	// 	// 			}
+	// 	// 			echo "</tr>";
+	// 	// 			}
+
+	// 	// 	echo "</table>";
+
+	//     // вывод результатов ввиде массива
+	// 	    // echo "<br><b> result_query_SQL: </b>&nbsp;&nbsp;&nbsp;&nbsp;";
+	// 	    // print_r($result_query_SQL);
+
+
+		    // echo "<br><b> result: </b>&nbsp;&nbsp;&nbsp;&nbsp;<br>";
+		    // echo Build_tree_arr($result);
+
 
 
 
