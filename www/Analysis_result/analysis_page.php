cc1fd030151c53ba7e8cbb634f5eccd81ce332fb
Romron <romron@ukr.net>
2018-01-05 20:30:23 +0200

    1.0.2

--- www/Analysis_result/analysis_page.php
+++ www/Analysis_result/analysis_page.php
@@ -5,8 +5,9 @@
 	<meta charset="utf-8">
 	<meta description="Страница для тестов">
 
-	<link href="css/table_css.css" rel="stylesheet">
-	<?php 	include('funktions_hypmon.php');	?>
+	<link href="css/analysis_css.css" rel="stylesheet">
+	<?php 	require_once('funktions_hypmon.php');	?>
+	<?php 	require_once('funktions_analysis.php');	?>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script src="js/jquery-3.1.1.js"></script>
 
@@ -18,12 +19,10 @@
 		// $name_table = "Crypto_1";	//	Выбор таблицы в базе данных
 		$name_table = "Crypto_test";	//	Выбор таблицы в базе данных
 		// $name_table = "Work_table_1";	//	Выбор таблицы в базе данных
-		// $name_table = "Work_table_3";	//	Выбор таблицы в базе данных
 		$link_DB = conect_DB();	
 
 
 		$result = querySortingFromDB($link_DB,$name_table,'Name','Date','DESC','',1);
-		// $result = querySortingFromDB($link_DB,$name_table,'Name','Capitalization','DESC','',1);
 
 	    // echo "<br><b> result: </b>&nbsp;&nbsp;&nbsp;&nbsp;<br>";
 	    // echo Build_tree_arr($result);
---
+++ www/Analysis_result/funktions_analysis.php
@@ -0,0 +1,91 @@
+<?php
+
+	function querySortingFromDB($link_DB,$name_table,$main_field,$sorting_field,$sorting_direction='ASC',$WHERE='',$table=false){	//	Данная функция извликает данные из базы, групирует и сортирует их по указаным параметрам
+
+	   $patern_zero = '~\d\.[1-9]*(0*)$~';
+	   $patern_zero = '~\.\d*[1-9]?(0*)(?<=$)~U';
+
+	    if ($WHERE == '') {
+		    $query_1 = "SELECT * FROM `".$name_table."` 		
+		    			GROUP BY `".$main_field."`
+		    			ORDER BY `".$sorting_field."` ".$sorting_direction."
+		    			";	 
+		    }else{   
+			    $query_1 = "SELECT * FROM `".$name_table."` 		
+			    			WHERE `".$sorting_field."` > ".$WHERE."
+			    			GROUP BY `".$main_field."`
+			    			ORDER BY `".$sorting_field."` ".$sorting_direction."
+			    			";	  	
+			    }
+
+	    // отправка ПЕРВОГО запроса 
+		   	$result_query_SQL = mysqli_query($link_DB,$query_1);
+		    if (!$result_query_SQL) {
+		    	echo "<br><br><b><font color='red'>".__FUNCTION__."&nbsp; Query_1 failed : " . mysqli_error($link_DB)."</font></b>";	
+		    	exit();
+		    	}     
+
+	    // оброботка ПЕРВОГО ответа сервера
+			for ($i=0; $i < mysqli_num_rows($result_query_SQL); $i++) {
+		    	if ($i>20) { break; }	// для тестов
+		   		$result_1[] = mysqli_fetch_assoc($result_query_SQL);
+		    	}
+
+		// формируем и отправляем ВТОРОЙ запрос
+			for ($z=0; $z < count($result_1); $z++) { 
+			  	if ($z>20) { break; }	// для тестов
+			    $query_2 = "SELECT * FROM `".$name_table."` 
+			    			WHERE `".$main_field."` = '".$result_1[$z][$main_field]."'
+			    			";	
+
+			    // отправка ВТОРОГО запроса 
+				   	$result_query_SQL = mysqli_query($link_DB,$query_2);
+				    if (!$result_query_SQL) {
+				    	echo "<br><br><b><font color='red'>".__FUNCTION__."&nbsp; Query_2 failed : " . mysqli_error($link_DB)."</font></b>";	
+				    	exit();
+				    	}    
+
+			    // оброботка ВТОРОГО ответа сервера
+					for ($i=0; $i < mysqli_num_rows($result_query_SQL); $i++) {
+				   		$result_2[] = mysqli_fetch_assoc($result_query_SQL);
+				    	}
+				}
+
+		// вывод результатов в виде таблицы
+		if ($table) {
+			echo "<table>";
+				foreach ($result_2[0] as $key => $value) {
+					if ($key == $sorting_field) {
+						$th_str = '<th class="sorting">';
+					}elseif ($key == $main_field){
+						$th_str = '<th class="main_sorting">';
+						}else{$th_str = '<th>';}					
+					echo $th_str.$key."</th>";
+					}
+
+				for ($q=0; $q < count($result_2); $q++) { 
+					echo "<tr>";
+					foreach ($result_2[$q] as $key_2 => $value_2) {
+						if (preg_match_all($patern_zero,$value_2,$result_zero,PREG_PATTERN_ORDER)) { // убираем незначущие нули после запятой
+							$value_2 = str_replace($result_zero[1][0],"",$value_2);	
+							$value_2 = $value_2.'0';
+							}
+						if ($key_2 == $sorting_field) {
+							$td_str = '<td class="sorting">';
+						}elseif ($key_2 == $main_field){
+							$td_str = '<td class="main_sorting">';
+							}else{$td_str = '<td>';}
+						echo $td_str;
+							echo $value_2;
+						echo "</td>";
+						}
+					echo "</tr>";
+					}
+			echo "</table>";
+	   		}
+	   	return $result_2;
+		}
+
+
+
+?>
