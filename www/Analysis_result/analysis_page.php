032b583d6df2726973028c11ad93c1d6a4303b41
Romron <romron@ukr.net>
2018-01-06 16:59:29 +0200

    1.0.5

--- www/Analysis_result/analysis_page.php
+++ www/Analysis_result/analysis_page.php
@@ -20,23 +20,56 @@
 			HEDER
 		</div>
 
+		<div class="content block_switch">
+			BLOCK_SWITCH
+		</div>
+
 		<div class="content">
 			<?php
-
-				// echo "CONTENT";
 
 				// $name_table = "Crypto_1";	//	Выбор таблицы в базе данных
 				$name_table = "Crypto_test";	//	Выбор таблицы в базе данных
 				// $name_table = "Work_table_1";	//	Выбор таблицы в базе данных
 				$link_DB = conect_DB();	
-
-
-				$result = querySortingFromDB($link_DB,$name_table,'Name','Date','DESC','',1);
-
-				   // echo "<br><b> result: </b>&nbsp;&nbsp;&nbsp;&nbsp;<br>";
-				   // echo Build_tree_arr($result);
-
+				$result = querySortingFromDB($link_DB,$name_table,'Name','Date','DESC','',0);
 			?>
+			
+			<div class="table">
+			<?php echo "<table>"; ?>
+				<div class="thed">
+					<?php 
+					
+						foreach ($result[0] as $key => $value) {
+							if ($key == $sorting_field) {
+								$th_str = '<th class="sorting">';
+							}elseif ($key == $main_field){
+								$th_str = '<th class="main_sorting">';
+								}else{$th_str = '<th>';}					
+							echo $th_str.$key."</th>";
+							}
+					?>
+				</div>
+				<div class="tbody">
+					<?php 
+						for ($q=0; $q < count($result); $q++) { 
+							echo "<tr>";
+							foreach ($result[$q] as $key_2 => $value_2) {
+								// if ($key_2 == $sorting_field) {
+								// 	$td_str = '<td class="sorting">';
+								// }elseif ($key_2 == $main_field){
+								// 	$td_str = '<td class="main_sorting">';
+								// 	}else{$td_str = '<td>';}
+								// echo $td_str;
+								echo "<td>";	
+									echo $value_2;
+								echo "</td>";
+								}
+							echo "</tr>";
+							}
+					?> 
+				</div>
+			<?php echo "</table>";  ?>
+			</div>
 		</div>
 
 		<div class="footer">
